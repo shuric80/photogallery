96a07f43bcfd08dd1e8bc2ad45b802f06c30f72f
shuric <shuric80@gmail.com>
2016-10-16 16:02:28 +0300

    config angular

--- client/js/app-route.js
+++ client/js/app-route.js
@@ -2,18 +2,23 @@
     'use strict';
     angular
         .module('app.route',['ui.router'])
-        .config(function ($stateProvider, $urlRouterProvider){
-            $urlRouterProvider.otherwise('/');
-            $stateProvider
-                .state('test', {
-                    url:'/test',
-                    views:{
-                        '':{
-                            templateUrl:'static/js/test.html',
-                            controller:'IndexController'
-                        }
+        .config(config);
+    
+    function config ($stateProvider, $urlRouterProvider, $interpolateProvider){
+        $interpolateProvider.startSymbol('{[');
+        $interpolateProvider.endSymbol(']}');
+
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('test', {
+                url:'/test',
+                views:{
+                    '':{
+                        templateUrl:'static/js/test.html',
+                        controller:'IndexController'
                     }
                 }
-                      )
-        })
+            }
+                  )
+    }
 })()
